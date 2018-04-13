8b5ab318de72b069dd17ffb831b6fbd7f985c15f
Tom Ellis <toelli@microsoft.com>
2018-04-13 19:24:23 +0100

    Jsonargs2

---
+++ Jsonargs2.hs
@@ -0,0 +1,145 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Aeson as A
+import           Data.Text (Text)
+import qualified Data.HashMap.Strict as HM
+import qualified Data.Set
+import           Data.Scientific (Scientific)
+
+data Schema a where
+  SMap    :: (a -> b) -> Schema a -> Schema b
+  SString :: Maybe Text -> Schema Text
+  SNumber :: Maybe Scientific -> Schema Scientific
+  SOneOf  :: SchemaOneOf a -> Schema a
+  SAllOf  :: SchemaAllOf a -> Schema a
+
+
+data SchemaOneOf a where
+  SchemaOneOf :: [(Text, Schema a)] -> SchemaOneOf a
+  SchemaOneOfDefault :: (Text, Schema a) -> [(Text, Schema a)] -> SchemaOneOf a
+
+data SchemaAllOf a where
+  AllField :: Text -> Schema a -> SchemaAllOf a
+  Apply :: SchemaAllOf (a -> b) -> SchemaAllOf a -> SchemaAllOf b
+  Pure :: a -> SchemaAllOf a
+
+
+
+merge :: Schema a -> Maybe A.Value -> Maybe a
+merge = \case
+    SMap f s'     -> fmap f . merge s'
+    SString mText -> \case
+      Nothing -> mText
+      Just (A.String t) -> Just t
+      _          -> Nothing
+    SNumber mNumber -> \case
+      Nothing -> mNumber
+      Just (A.Number n) -> Just n
+      _          -> Nothing
+    SOneOf s' -> mergeSchemaOneOf s'
+    SAllOf s' -> mergeSchemaAllOf s'
+
+mergeSchemaOneOf :: SchemaOneOf a -> Maybe A.Value -> Maybe a
+mergeSchemaOneOf = \case
+  SchemaOneOf l -> \case
+    Nothing -> Nothing
+    Just a  -> case a of
+      A.Object hm -> case HM.toList hm of
+        []      -> Nothing
+        (_:_:_) -> Nothing
+        [(field, fieldOther)] -> do
+          schema <- lookup field l
+          merge schema (Just fieldOther)
+      _ -> Nothing
+  SchemaOneOfDefault (defField, defSchema) l -> \case
+    Nothing -> merge defSchema Nothing
+    Just a -> case a of
+      A.Object hm -> case HM.toList hm of
+        [] -> merge defSchema Nothing
+        (_:_:_) -> Nothing
+        [(field, fieldOther)] -> do
+          schema <- lookup field ((defField, defSchema) : l)
+          merge schema (Just fieldOther)
+      _ -> Nothing
+
+mergeSchemaAllOf' :: SchemaAllOf a -> (HM.HashMap Text A.Value -> Maybe a, [Text])
+mergeSchemaAllOf' = \case
+  Pure a -> (const (Just a), [])
+  Apply x y -> case mergeSchemaAllOf' x of
+    (gx, lx) -> case mergeSchemaAllOf' y of
+      (gy, ly) -> (\v -> gx v <*> gy v, lx ++ ly)
+  AllField t schema -> (merge schema . HM.lookup t, [t])
+
+mergeSchemaAllOf :: SchemaAllOf a -> Maybe A.Value -> Maybe a
+mergeSchemaAllOf s = \case
+  Nothing -> f HM.empty
+  Just (A.Object hm) -> if Data.Set.fromList (HM.keys hm)
+                           `Data.Set.isSubsetOf`
+                           Data.Set.fromList ts
+                        then f hm
+                        else Nothing
+  Just _ -> Nothing
+
+  where (f, ts) = mergeSchemaAllOf' s
+
+
+data ComputeTarget = GPU Scientific Text | CPU Scientific deriving Eq
+
+computeTarget = SOneOf (SchemaOneOfDefault ("gpu", gpu)
+                                           [("cpu", cpu)])
+
+cpu = SAllOf (AllField "num_cpus" (SMap CPU num_cpus))
+
+gpu = SAllOf (Pure GPU `Apply` AllField "gpu_id" gpu_id
+                       `Apply` AllField "cluster" cluster)
+
+gpu_id = SNumber (Just 0)
+cluster = SString (Just "local")
+num_cpus = SNumber (Just 1)
+
+
+data Size = Large | Small deriving Eq
+
+int0 = SNumber (Just 0)
+
+size = SOneOf (SchemaOneOfDefault ("large", SMap (const Large) int0)
+                                  [ ("small", SMap (const Small) int0) ])
+
+main :: IO ()
+main = do
+  assert $ merge num_cpus Nothing == Just 1
+  assert $ merge cluster Nothing  == Just "local"
+  assert $ merge cpu Nothing == Just (CPU 1)
+
+  assert $ merge num_cpus (Just (A.Number 2)) == Just 2
+  assert $ merge cluster (Just (A.String "remote")) == Just "remote"
+  assert $ merge cpu (Just (A.Object (HM.singleton "num_cpus" (A.Number 2)))) == Just (CPU 2)
+  assert $ merge cpu (Just (A.Object (HM.singleton "shouldn't exist" (A.Number 2)))) == Nothing
+
+  assert $ merge cpu (Just (A.Object HM.empty)) == Just (CPU 1)
+  assert $ merge gpu (Just (A.Object (HM.singleton "gpu_id" (A.Number 5)))) == Just (GPU 5 "local")
+
+  assert $ merge size Nothing == Just Large
+
+  let (.=) = (,)
+      d = A.Object . HM.fromList
+      n = A.Number
+
+  let ct_defaults = [ (Nothing, GPU 0 "local")
+                    , (Just (A.Object (HM.singleton "gpu" (A.Object HM.empty))), GPU 0 "local")
+                    , (Just (d ["gpu" .= d ["gpu_id" .= A.Number 1]]), GPU 1 "local")
+                    , (Just (d ["cpu" .= d []]), CPU 1)
+                    , (Just (d ["cpu" .= d ["num_cpus" .= A.Number 5]]), CPU 5)
+                    ]
+
+  flip mapM_ ct_defaults $ \(ct_default, ct_expected) -> assert $ merge computeTarget ct_default == Just ct_expected
+
+  putStrLn ""
+  putStrLn ""
+  putStrLn "Success!"
+  putStrLn ""
+  putStrLn ""
+
+  where assert b = if b then return () else error "Assertion failure"
